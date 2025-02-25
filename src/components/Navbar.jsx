0fcdb442e03eb57d29474dbbbee08dfacb7a9242
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-02-25 23:11:12 +0200

    Added profile.webp

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,42 @@
-import React from "react";
-
+import logo from "../assets/raviKumarLogo.webp";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <a href="/" aria-label="Home">
+          <img src={logo} className="mx-2" width={50} height={33} alt="Logo" />
+        </a>
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a
+          href="https://www.linkedin.com/in/cedrick-bienvenue-256317298/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="LinkedIn"
+        >
+          <FaLinkedin />
+        </a>
+
+        <a
+          href="https://www.linkedin.com/in/cedrick-bienvenue-256317298/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="GitHub"
+        >
+          <FaLinkedin />
+        </a>
+
+        <a
+          href="https://www.linkedin.com/in/cedrick-bienvenue-256317298/"
+          target="_blank"
+          rel="noopener noreferrer"
+          aria-label="LinkedIn"
+        >
+          <FaLinkedin />
+        </a>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
