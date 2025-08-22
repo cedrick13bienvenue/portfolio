3b2c89f7709eba36491cc6e521dc626cd3439d05
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-08-22 10:21:41 +0200

    feat: integrate AI chatbot into main application layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import Technologies from "./components/Technologies";
+import Chatbot from "./components/Chatbot";
 
 const App = () => {
   return (
@@ -24,6 +25,9 @@ const App = () => {
         <Experience />
         <Contact />
       </div>
+
+      {/* Chatbot Component */}
+      <Chatbot />
     </div>
   );
 };
